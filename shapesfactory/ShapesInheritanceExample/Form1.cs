205ffb8ec804caad713c85c9229c2e5e430b32ae
dmullier <d.mullier@leedsbeckett.ac.uk>
2022-11-08 14:15:30 +0000

    Add files via upload
    Initial project upload.

---
+++ shapesfactory/ShapesInheritanceExample/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ShapesInheritanceExample
+{
+    public partial class Form1 : Form 
+    {
+        ArrayList shapes = new ArrayList();
+        
+        
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ShapeFactory factory = new ShapeFactory();
+            try
+            {
+                shapes.Add(factory.getShape("circle"));
+                shapes.Add(factory.getShape("triangle"));
+                shapes.Add(factory.getShape("rectangle"));
+                
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Invalid shape: " + e);
+                
+            }
+
+            
+            
+
+            //add some random shapes
+            Random rand = new Random(77887);
+            for (int i=0; i<150; i++)
+            {
+                int x = rand.Next(Size.Width);
+                int y = rand.Next(Size.Height);
+                int size = rand.Next(250);
+
+                int red = rand.Next(255);
+                int green = rand.Next(255);
+                int blue = rand.Next(255);
+
+                Color newColour = Color.FromArgb(128, red, green, blue); //128 is semi transparent
+
+                int shape = rand.Next(4);
+                Shape s;
+                try
+                {
+                    switch (shape)
+                    {
+                        case 0:
+                            s = factory.getShape("circle");
+                            s.set(newColour, x, y, size);
+                            shapes.Add(s);// new Circle(newColour, x, y, size));
+
+                            break;
+                        case 1:
+                            s = factory.getShape("rectangle");
+                            s.set(newColour, x, y, size, size);
+                            shapes.Add(s);
+                            break;
+                        case 2:
+                            s = factory.getShape("square");
+                            s.set(newColour, x, y, size, size);
+                            shapes.Add(s);
+                            break;
+                        case 3:
+                            s = factory.getShape("tritangle");
+                            s.set(newColour, x, y, size, size);
+                            shapes.Add(s);
+                            break;
+
+                    }
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine("Invalid shape: " + e);
+
+                }
+            }
+            
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Console.WriteLine("paint called");
+            Graphics g = e.Graphics;
+         
+
+            for (int i = 0; i<shapes.Count; i++)
+            {
+                Shape s;
+                s = (Shape) shapes[i];
+                if (s != null)
+                {
+                    s.draw(g);
+                    Console.WriteLine(s.ToString());
+                }
+                else
+                    Console.WriteLine("invalid shape in array"); //shouldn't happen as factory does not produce rubbish
+
+               
+
+
+            }
+        }
+    }
+}
+
+
---
+++ shapesfactory/ShapesInheritanceExample/shape.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ShapesInheritanceExample
+{
+    abstract class Shape:Shapes
+    {
+        protected Color colour; //shape's colour
+        protected int x, y; //not I could use c# properties for this
+        public Shape()
+        {
+            colour = Color.Red;
+            x = y = 100;
+        }
+
+      
+        public Shape(Color colour, int x, int y)
+        {
+
+            this.colour = colour; //shape's colour
+            this.x = x; //its x pos
+            this.y = y; //its y pos
+            //can't provide anything else as "shape" is too general
+        }
+
+        //the three methods below are from the Shapes interface
+        //here we are passing on the obligation to implement them to the derived classes by declaring them as abstract
+        public virtual void draw(Graphics g)
+        {
+            Font drawFont = new Font("Arial", 12);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            // Set format of string.
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.NoClip;
+            String text = this.ToString();
+            g.DrawString(text, drawFont, drawBrush,this.x, this.y, drawFormat);
+        }
+        public abstract double calcArea(); //any derrived class must implement this method
+        public abstract double calcPerimeter(); //any derrived class must implement this method
+
+        //set is declared as virtual so it can be overridden by a more specific child version
+        //but is here so it can be called by that child version to do the generic stuff
+        //note the use of the param keyword to provide a variable parameter list to cope with some shapes having more setup information than others
+        public  virtual void set(Color colour, params int[] list)
+        {
+            this.colour = colour;
+            this.x = list[0];
+            this.y = list[1];
+        }
+
+
+        public override string ToString() //get the standard object name and break hierarchy to get just the name
+        {
+            String text = base.ToString();
+            String[] sut = text.Split('.');
+            text = sut[sut.Length - 1];
+            return text;
+        }
+
+    }
+}
+
+
